ab5e2ed6820d82241a9b28c3e3cd880f6d1d5022
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2022-08-07 19:19:56 +0900

    クラスターのタグを追加

--- modules/ecs/cluster/outputs.tf
+++ modules/ecs/cluster/outputs.tf
@@ -2,3 +2,8 @@ output "cluster_arn" {
   description = "ECSクラスターのARN"
   value = aws_ecs_cluster.cluster.arn
 }
+
+output "cluster_tags" {
+  description = "ECSクラスターのタグ"
+  value = aws_ecs_cluster.cluster.tags
+}
